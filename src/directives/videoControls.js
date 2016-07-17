16e044e6eeafd202ff72da32c758a6c2b680dd11
Pankaj Anupam <info@pankajanupam.in>
2016-07-17 17:16:20 +0530

    add videoControls directive
    enable / disable video controls, autoplay, muted etc

---
+++ src/directives/videoControls.js
@@ -0,0 +1,21 @@
+angular.module('ngpWebcamRecorder')
+    .directive('videoControls', [
+        function () {
+            return {
+                restrict: 'A',
+                link: function (scope, element, attrs) {
+                    /**
+                     * Watch for scope changes and add remove controls attribute 
+                     */
+                    scope.$watch(
+                        function () {
+                            
+                            scope.isMute ? element.removeAttr('muted') : element.attr('muted', 'muted');
+                            scope.autoPlay ? element.attr('autoplay', 'autoplay') : element.removeAttr('autoplay');
+                            scope.showControls ? element.attr('controls', 'controls') : element.removeAttr('controls');
+                        }
+                    );
+                }
+            };
+        }
+    ]);
